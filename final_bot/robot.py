957fbb88688d17e005a3ea5d6a829877615b261e
Vishwas_N_S <vishwas3151999@gmail.com>
2019-01-16 23:11:13 +0530

    Added Pilgrim logic

--- final_bot/robot.py
+++ final_bot/robot.py
@@ -30,6 +30,35 @@ class MyRobot(BCAbstractRobot):
 
     destination = None
 
+    def get_closest_resource(self,map,x,y): #Gives x,y of closest resource
+        loc = [x,y]
+        closest_loc = (-1, -1)
+        best_dist_sq = 64 * 64 + 64 * 64 + 1
+        for x in range(len(map)):
+            if (x - loc[0])**2 > best_dist_sq:
+                continue
+            for y in range(len(map[0])):
+                if (y - loc[1])**2 > best_dist_sq:
+                    continue
+                d = (x-loc[0]) ** 2 + (y-loc[1]) **2
+                if map[y][x] and d < best_dist_sq:
+                    best_dist_sq = d
+                    closest_loc = (x,y)
+        return closest_loc
+     
+    def move_to(self,x,y,passable_map,visible_map): #moves "smartly" to [x,y]. Make sure x,y is passable
+        return None
+
+    def get_path(self,x,y,passable_map): #returns path to [x,y], tile by tile
+        return []
+
+    def is_adjacent(self,r): #Returns true if 'r' object is adjacent to 'self' object
+        l = [r.x-self.x,r.y-self.y]
+        for i in self.surrounding_tiles:
+            if(l == i):
+                return True
+        return False
+
     def turn(self):
         self.round += 1
 
@@ -50,18 +79,20 @@ class MyRobot(BCAbstractRobot):
                         return self.build_unit(SPECS['PILGRIM'],dx,dy)
 
         if self.me['unit'] == SPECS['PILGRIM']:
+            
             if self.destination == None:
-                self.destination = get_closest_resource(karbonite_map,self.x,self.y)
+                self.destination = self.get_closest_resource(karbonite_map,self.x,self.y)
+            if self.destination_path == None:
+                self.destination_path = self.get_path(*self.destination,passable_map)
 
-            if self.me.karbonite == SPECS['UNITS'][SPECS["PILGRIM"]]['KARBONITE_CAPACITY'] and :
-                pass
+            if self.me.karbonite == SPECS['UNITS'][SPECS["PILGRIM"]]['KARBONITE_CAPACITY']:
+                for r in visible_robots:
+                    if(r.team == self.me.team and r.id == SPECS['CRUSADER'] and self.is_adjacent(r)):
+                        return self.give(r.x-self.x,r.y-self.y,self.me.karbonite,0)
+
             if(self.x == self.destination[0] and self.y == self.destination[1]):
-                self.mine()
+                return self.mine()
 
-    def get_closest_resource(self,map,x,y):
-        pass
-
-    def move_to(self,x,y,passable_map,visible_map):
-        pass
+        
 
 robot = MyRobot()
